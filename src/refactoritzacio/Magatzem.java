76e4f1fd6de0857e0b8ab3da0ed56aa34bc8024e
Alejandro Molina <fdo.molina-117@hotmail.es>
2026-05-14 09:42:45 +0200

    Divideix la logica dels articles per facilitar el polimorfisme
    S'ha separat la logica de cada tipus d'article en metodes diferents per fer el codi mes clar i facilitar futures millores amb polimorfisme.

--- src/refactoritzacio/Magatzem.java
+++ src/refactoritzacio/Magatzem.java
@@ -1,62 +1,129 @@
 package refactoritzacio;
 
 class Magatzem {
+
+    /* Array que guarda tots els articles del magatzem */
     Article[] articles;
 
     public Magatzem(Article[] articles) {
         this.articles = articles;
     }
 
+    /*
+     * Recorre tots els articles
+     * i actualitza el seu estat
+     */
     public void actualitzarEstat() {
+
         for (int i = 0; i < articles.length; i++) {
-            if (!articles[i].nom.equals("Formatge Gidurat")
-                    && !articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                if (articles[i].qualitat > 0) {
-                    if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                        articles[i].qualitat = articles[i].qualitat - 1;
-                    }
-                }
-            } else {
-                if (articles[i].qualitat < 50) {
-                    articles[i].qualitat = articles[i].qualitat + 1;
+            actualitzarArticle(articles[i]);
+        }
+    }
 
-                    if (articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].diesPerVendre < 11) {
-                            if (articles[i].qualitat < 50) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
-                            }
-                        }
+    /*
+     * Comprova el tipus d'article
+     * i crida el mètode corresponent
+     */
+    private void actualitzarArticle(Article article) {
 
-                        if (articles[i].diesPerVendre < 6) {
-                            if (articles[i].qualitat < 50) {
-                                articles[i].qualitat = articles[i].qualitat + 1;
-                            }
-                        }
-                    }
+        if (article.nom.equals("Martell de Thor (Llegendari)")) {
+
+            actualitzarMartell(article);
+
+        } else if (article.nom.equals("Formatge Gidurat")) {
+
+            actualitzarFormatge(article);
+
+        } else if (article.nom.equals("Entrades per al Concert del Trobador")) {
+
+            actualitzarEntrades(article);
+
+        } else {
+
+            actualitzarArticleNormal(article);
+        }
+    }
+
+    /*
+     * Articles normals:
+     * - la qualitat baixa cada dia
+     * - quan caduca, baixa el doble
+     */
+    private void actualitzarArticleNormal(Article article) {
+
+        if (article.qualitat > 0) {
+            article.qualitat--;
+        }
+
+        article.diesPerVendre--;
+
+        if (article.diesPerVendre < 0) {
+
+            if (article.qualitat > 0) {
+                article.qualitat--;
+            }
+        }
+    }
+
+    /*
+     * El formatge augmenta la qualitat
+     * amb el pas del temps
+     */
+    private void actualitzarFormatge(Article article) {
+
+        if (article.qualitat < 50) {
+            article.qualitat++;
+        }
+
+        article.diesPerVendre--;
+
+        if (article.diesPerVendre < 0) {
+
+            if (article.qualitat < 50) {
+                article.qualitat++;
+            }
+        }
+    }
+
+    /*
+     * Les entrades augmenten la qualitat
+     * segons la proximitat del concert
+     *
+     * Després del concert la qualitat passa a 0
+     */
+    private void actualitzarEntrades(Article article) {
+
+        if (article.qualitat < 50) {
+
+            article.qualitat++;
+
+            if (article.diesPerVendre < 11) {
+
+                if (article.qualitat < 50) {
+                    article.qualitat++;
                 }
             }
 
-            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                articles[i].diesPerVendre = articles[i].diesPerVendre - 1;
-            }
+            if (article.diesPerVendre < 6) {
 
-            if (articles[i].diesPerVendre < 0) {
-                if (!articles[i].nom.equals("Formatge Gidurat")) {
-                    if (!articles[i].nom.equals("Entrades per al Concert del Trobador")) {
-                        if (articles[i].qualitat > 0) {
-                            if (!articles[i].nom.equals("Martell de Thor (Llegendari)")) {
-                                articles[i].qualitat = articles[i].qualitat - 1;
-                            }
-                        }
-                    } else {
-                        articles[i].qualitat = articles[i].qualitat - articles[i].qualitat;
-                    }
-                } else {
-                    if (articles[i].qualitat < 50) {
-                        articles[i].qualitat = articles[i].qualitat + 1;
-                    }
+                if (article.qualitat < 50) {
+                    article.qualitat++;
                 }
             }
         }
+
+        article.diesPerVendre--;
+
+        if (article.diesPerVendre < 0) {
+            article.qualitat = 0;
+        }
+    }
+
+    /*
+     * El Martell és un article llegendari
+     * i no modifica el seu estat
+     */
+    private void actualitzarMartell(Article article) {
+
     }
 }
